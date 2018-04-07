bb5a93854cfa359853d01f7584521d0bbf14f0fc
Screencasts <screencasts@Nathaniels-MacBook-Pro-2.local>
2018-04-07 13:04:39 -0700

    add bash configuration

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
 git clone https://github.com/nathanielks/dotfiles ~/.dotfiles
 (cd ~/.dotfiles && bin/install-symlinks.sh)
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
