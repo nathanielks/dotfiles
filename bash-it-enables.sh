2562291b9d399e276c720350ca776038bdedd1dd
Nathaniel Schweinberg <natanielks@gmail.com>
2018-04-07 13:40:31 -0700

    add some git aliases, script to enable completions and aliases

---
+++ bash-it-enables.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+source "$HOME/.bashit_init"
+
+for a in curl git general vagrant
+do
+  bash-it enable alias "$a"
+done
+
+for c in 'bash-it' brew composer dirs git ssh system vagrant virtualbox
+do
+  bash-it enable completion "$c"
+done
