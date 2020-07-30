571d8bbc098f70d6810d415ec7f2c229c08e3a0a
Nathaniel Schweinberg <314009+nathanielks@users.noreply.github.com>
2020-07-29 21:31:44 -0500

    added a full-install script to codify most everything

---
+++ full-install.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Python
+cbrew install pyenv
+pyenv install 3.7.5
+pyenv global 3.7.5
+pyenv version
+
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# NodeJS
+brew install n
+n lts
+
+# Terraform
+brew install tfenv
+
+# Neovim
+brew install neovim
+sudo -H pip install neovim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Everything else
+brew install ansible
+( cd ~/.dotfiles && ansible-playook -i hosts computer.yml )
