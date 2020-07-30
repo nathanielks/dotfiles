ce183e4a66084978d9b7cd387c0470c2177e05a7
Nathaniel Schweinberg <314009+nathanielks@users.noreply.github.com>
2020-07-29 20:04:10 -0500

    move evals to zshrc

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -28,14 +28,11 @@ function tfa(){
 	tf apply plan $*
 }
 
-eval $(thefuck --alias)
-eval "$(hub alias -s)"
-
 # Go forward in Git commit hierarchy, towards particular commit
 # Usage:
 #  gofwd v1.2.7
 # Does nothing when the parameter is not specified.
-gofwd() {
+function gofwd() {
   git checkout `git rev-list --topo-order HEAD.."$*" | tail -1`
 }
 
