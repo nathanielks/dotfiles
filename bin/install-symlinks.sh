22c7884616f39efe0b5f07b25fa2803a605ef146
Nathaniel Schweinberg <314009+nathanielks@users.noreply.github.com>
2020-07-29 20:02:02 -0500

    enable adding symlinks to deeper directories

--- bin/install-symlinks.sh
+++ bin/install-symlinks.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
+DOTFILES_DIR="$( cd "$( dirname "$0" )" && pwd )/../"
+echo $DOTFILES_DIR
+
 for FILE in **/*.symlink; do
   SOURCE="$(pwd)/$FILE"
   FILENAME=$(basename "$FILE" ".symlink")
   TARGET="$HOME/.$FILENAME"
   ln -sif "$SOURCE" "$TARGET"
 done
+
+mkdir -p ~/.config/fish
+ln -sf "$DOTFILES_DIR/fish/config.fish" ~/.config/fish/config.fish
