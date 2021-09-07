bfae77bc45026598496fed2bef48a2c3c5940b39
Nathaniel Schweinberg <314009+nathanielks@users.noreply.github.com>
2021-09-07 15:49:15 -0500

    save keyboard settings

--- pre-install.sh
+++ pre-install.sh
@@ -3,5 +3,7 @@
 # If macOS
 if [[ "$OSTYPE" == "darwin"* ]]; then
   defaults write -g AppleShowAllFiles -bool true
-# elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  # Set right alt to win for Regolith
+  setxkbmap -option altwin:swap_alt_win
 fi
